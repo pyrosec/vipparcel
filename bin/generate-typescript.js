c41695139c1d4bcdd79c2f9d5575a825225e959a
pyrosec <inquiries@pyrosec.gg>
2023-10-25 22:53:00 +0000

    update

--- bin/generate-typescript.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const schema = require('../schema');
-const path = require('path');
-schema.forEach((v) => {
-  const parts = v.url.split('/');
-  let param = parts.find((v) => v.match('{'));
-  if (param) param = param.substr(1, param.length - 2);
-  console.log('  async ' + parts.
